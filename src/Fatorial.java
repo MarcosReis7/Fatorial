11e921277d9d75c7ef99ee46154af200eb9c013a
MarcosReis7 <mreis6357133@gmail.com>
2021-10-04 22:40:46 -0300

    programa usado para criar lógicas de repetição

---
+++ src/Fatorial.java
@@ -0,0 +1,24 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Fatorial {
+	
+	public static void main(String[] args) {
+		int i = 0;
+		
+		while (i <= 48) {
+			int number1 = 1;
+			int number2 = 1;
+			if (number1 == number2) {
+				System.out.print("bt1.addActionListener(new ActionListener() {\r\n"
+						+ "			public void actionPerformed(ActionEvent e) {\r\n"
+						+ "				\r\n"
+						+ "			}\r\n"
+						+ "		});"+"\n");
+				i += 1;
+				
+			}
+		}
+			
+	}
+}
